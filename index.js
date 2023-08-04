44b966f77595c654abefa1667a515459c667e244
rafael moreira <rafa.siffert@gmail.com>
2023-08-03 22:29:34 -0400

    Dodger game

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.left = "180px";
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    const gameContainerWidth = 400; // Adjust this value if needed
+    const dodgerWidth = 40; // Adjust this value if needed
+  
+    if (left < gameContainerWidth - dodgerWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
